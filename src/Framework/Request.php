428f7b9239723b09f89dffc5bbeb2318257338da
冷斌 <xykxyk2008@163.com>
2020-08-22 23:08:29 +0800

    init

--- src/Framework/Request.php
+++ src/Framework/Request.php
@@ -3,8 +3,21 @@
 namespace Lengbin\Hyperf\Common\Framework;
 
 use Hyperf\HttpServer\Request as BaseRequest;
+use Lengbin\Hyperf\Common\Framework\Exception\MethodNotImplException;
 
 class Request extends BaseRequest
 {
-
+    /**
+     * 真实 ip
+     *
+     * @param string $headerName
+     *
+     * @return mixed|string
+     *
+     * @throws MethodNotImplException
+     */
+    public function clientRealIP($headerName = 'x-real-ip')
+    {
+        throw new MethodNotImplException();
+    }
 }
