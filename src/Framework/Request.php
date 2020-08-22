3b014ad835102bb8ea802c9ecf46dcf21da689c3
冷斌 <xykxyk2008@163.com>
2020-08-22 23:16:40 +0800

    真实 ip

--- src/Framework/Request.php
+++ src/Framework/Request.php
@@ -12,12 +12,24 @@ class Request extends BaseRequest
      *
      * @param string $headerName
      *
-     * @return mixed|string
-     *
-     * @throws MethodNotImplException
+     * @return string
      */
     public function clientRealIP($headerName = 'x-real-ip')
     {
-        throw new MethodNotImplException();
+        $client = $this->getServerParams();
+        $clientAddress = $client['remote_ip'];
+        $xri = $this->getHeader($headerName);
+        $xff = $this->getHeader('x-forwarded-for');
+        if ($clientAddress === '127.0.0.1') {
+            if (!empty($xri)) {  // 如果有xri 则判定为前端有NGINX等代理
+                $clientAddress = $xri[0];
+            } elseif (!empty($xff)) {  // 如果不存在xri 则继续判断xff
+                $list = explode(',', $xff[0]);
+                if (isset($list[0])) {
+                    $clientAddress = $list[0];
+                }
+            }
+        }
+        return $clientAddress;
     }
 }
