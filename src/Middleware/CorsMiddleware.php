201d5cc38823d35cb5d4fa779c4edb87602b6859
冷斌 <xykxyk2008@163.com>
2020-10-18 23:17:18 +0800

    fix code bug

--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -26,18 +26,24 @@ class CorsMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $response = Context::get(ResponseInterface::class);
-        $response = $response->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Credentials', 'true')
-            // Headers 可以根据实际情况进行改写。
-            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
+        $header = $request->getHeaderLine('Access-Control-Request-Headers');
+        if (empty($header)) {
+            $header = '*';
+        }
 
+        // 设置跨域
+        $response = Context::get(ResponseInterface::class);
+        $response = $response->withAddedHeader('Access-Control-Expose-Headers', '*')
+            ->withAddedHeader('Access-Control-Allow-Origin', '*')
+            ->withAddedHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT  ')
+            ->withAddedHeader('Access-Control-Allow-Headers', $header);
         Context::set(ResponseInterface::class, $response);
 
-        if ($request->getMethod() == 'OPTIONS') {
+        if (strtoupper($request->getMethod()) == 'OPTIONS') {
             return $response;
         }
 
-        return $handler->handle($request);
+        $response = $handler->handle($request);
+        return $response;
     }
 }
