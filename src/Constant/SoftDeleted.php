bb41b658c71c670154d7026340ed4617c7a9e904
冷斌 <xykxyk2008@163.com>
2020-10-22 19:03:14 +0800

    upgrade error code

--- src/Constant/SoftDeleted.php
+++ src/Constant/SoftDeleted.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Lengbin\Hyperf\Common\Constant;
 
-use Lengbin\ErrorCode\BaseEnum;
+use Lengbin\Hyperf\ErrorCode\BaseEnum;
 
 class SoftDeleted extends BaseEnum
 {
--- src/Error/CommentErrorCode.php
+++ src/Error/CommentErrorCode.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Lengbin\Hyperf\Common\Error;
 
-use Lengbin\ErrorCode\BaseEnum;
+use Lengbin\Hyperf\ErrorCode\BaseEnum;
 
 class CommentErrorCode extends BaseEnum
 {
