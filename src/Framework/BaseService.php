d069ab7325f4ef65b146eafe66e21af43cc62366
冷斌 <xykxyk2008@163.com>
2021-01-05 16:54:01 +0800

    add gen-model for fix hyperf model gen

--- src/Framework/BaseService.php
+++ src/Framework/BaseService.php
@@ -49,6 +49,29 @@ class BaseService
     }
 
     /**
+     * page
+     *
+     * @param array      $params
+     * @param PageEntity $pageEntity
+     *
+     * @return array
+     */
+    public function pageByArray(array $params, PageEntity $pageEntity): array
+    {
+        $total = count($params);
+        $pageSize = $pageEntity->getPageSize();
+        $offset = ($pageEntity->getPage() - 1) * $pageSize;
+        $params = array_values($params);
+        $list = array_slice($params, $offset, $pageSize);
+        return [
+            'list'      => $list,
+            'pageSize'  => $pageSize,
+            'total'     => $total,
+            'totalPage' => ceil($total / $pageSize),
+        ];
+    }
+
+    /**
      * default
      *
      * @return Sort
