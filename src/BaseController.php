3a521891920832fcc40edec1cceff82339f1d975
冷斌 <xykxyk2008@163.com>
2022-02-20 12:59:11 +0800

    add column

--- src/BaseController.php
+++ src/BaseController.php
@@ -36,36 +36,4 @@ abstract class BaseController
         $this->request = $container->get(RequestInterface::class);
         $this->response = $container->get(Response::class);
     }
-
-    /**
-     * 真实 ip
-     *
-     * @param string $headerName
-     *
-     * @return mixed|string
-     */
-    public function getClientIp($headerName = 'x-real-ip')
-    {
-        $client = $this->request->getServerParams();
-        $xri = $this->request->getHeader($headerName);
-        if (!empty($xri)) {
-            $clientAddress = $xri[0];
-        } else {
-            $clientAddress = $client['remote_addr'];
-        }
-        $xff = $this->request->getHeader('x-forwarded-for');
-        if ($clientAddress === '127.0.0.1') {
-            if (!empty($xri)) {
-                // 如果有xri 则判定为前端有NGINX等代理
-                $clientAddress = $xri[0];
-            } elseif (!empty($xff)) {
-                // 如果不存在xri 则继续判断xff
-                $list = explode(',', $xff[0]);
-                if (isset($list[0])) {
-                    $clientAddress = $list[0];
-                }
-            }
-        }
-        return $clientAddress;
-    }
 }
