21fafccd474ececae786184b2b36fd9f4362f49f
fyi <fyi741@protonmail.com>
2022-06-02 16:53:11 +0800

    优化

--- src/Middlewares/DebugLogMiddleware.php
+++ src/Middlewares/DebugLogMiddleware.php
@@ -9,9 +9,12 @@ declare(strict_types=1);
 
 namespace Lengbin\Hyperf\Common\Middlewares;
 
+use Hyperf\Di\Annotation\Inject;
+use Lengbin\Hyperf\Common\Helpers\IpHelper;
 use Lengbin\Hyperf\Common\Logs\AppendRequestIdProcessor;
 use Hyperf\Snowflake\IdGenerator\SnowflakeIdGenerator;
 use Hyperf\Utils\Context;
+use Hyperf\Logger\LoggerFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -20,19 +23,47 @@ use Psr\Http\Server\RequestHandlerInterface;
 class DebugLogMiddleware implements MiddlewareInterface
 {
 
-    /**
-     * @var SnowflakeIdGenerator
-     */
+    #[Inject()]
     protected SnowflakeIdGenerator $idGenerator;
 
-    public function __construct(SnowflakeIdGenerator $idGenerator)
+    #[Inject()]
+    protected IpHelper $ipHelper;
+
+    protected LoggerFactory $loggerFactory;
+
+    public function __construct()
     {
-        $this->idGenerator = $idGenerator;
+       $this->loggerFactory = make(LoggerFactory::class);
+    }
+
+    /**
+     * 记录日志
+     */
+    protected function logger($data, string $name = 'hyperf'): void
+    {
+        if (is_array($data)) {
+            $data = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+        }
+        $this->loggerFactory->get($name)->info($data);
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         Context::getOrSet(AppendRequestIdProcessor::REQUEST_ID, $this->idGenerator->generate());
-        return $handler->handle($request);
+
+        // 记录请求日志
+        $this->logger([
+            'user-agent' => $request->getHeaderLine('user-agent'),
+            'ip' => $this->ipHelper->getClientIp(),
+            'host' => $request->getUri()->getHost(),
+            'url' => $request->getUri()->getPath(),
+            'post' => $request->getParsedBody(),
+            'get' => $request->getQueryParams(),
+        ], 'request');
+
+        $response = $handler->handle($request);
+
+        $this->logger($response->getBody()->getContents(), 'response');
+        return $response;
     }
 }
