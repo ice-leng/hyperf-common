7ef9b5a30ce74c67e48bf8102a41a2cccb9af295
fyi <fyi741@protonmail.com>
2022-06-02 17:05:07 +0800

    优化

--- src/Middlewares/DebugLogMiddleware.php
+++ src/Middlewares/DebugLogMiddleware.php
@@ -13,7 +13,7 @@ use Hyperf\Di\Annotation\Inject;
 use Lengbin\Hyperf\Common\Helpers\IpHelper;
 use Lengbin\Hyperf\Common\Logs\AppendRequestIdProcessor;
 use Hyperf\Snowflake\IdGenerator\SnowflakeIdGenerator;
-use Hyperf\Utils\Context;
+use Hyperf\Context\Context;
 use Hyperf\Logger\LoggerFactory;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -36,34 +36,23 @@ class DebugLogMiddleware implements MiddlewareInterface
        $this->loggerFactory = make(LoggerFactory::class);
     }
 
-    /**
-     * 记录日志
-     */
-    protected function logger($data, string $name = 'hyperf'): void
-    {
-        if (is_array($data)) {
-            $data = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-        }
-        $this->loggerFactory->get($name)->info($data);
-    }
-
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         Context::getOrSet(AppendRequestIdProcessor::REQUEST_ID, $this->idGenerator->generate());
 
         // 记录请求日志
-        $this->logger([
+        $this->loggerFactory->get('request')->info(json_encode([
             'user-agent' => $request->getHeaderLine('user-agent'),
             'ip' => $this->ipHelper->getClientIp(),
             'host' => $request->getUri()->getHost(),
             'url' => $request->getUri()->getPath(),
             'post' => $request->getParsedBody(),
             'get' => $request->getQueryParams(),
-        ], 'request');
+        ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
 
         $response = $handler->handle($request);
 
-        $this->logger($response->getBody()->getContents(), 'response');
+        $this->loggerFactory->get('response')->info($response->getBody()->getContents());
         return $response;
     }
 }
