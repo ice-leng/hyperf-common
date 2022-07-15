da6131879294203f6a8a457ab77581cb27a30d7a
fyi <fyi741@protonmail.com>
2022-07-15 14:37:41 +0800

    upgrade 3.0

--- src/Logs/AppendRequestIdProcessor.php
+++ src/Logs/AppendRequestIdProcessor.php
@@ -13,6 +13,7 @@ use Hyperf\Di\Annotation\Inject;
 use Hyperf\Snowflake\IdGenerator\SnowflakeIdGenerator;
 use Hyperf\Utils\Context;
 use Hyperf\Utils\Coroutine;
+use Monolog\LogRecord;
 use Monolog\Processor\ProcessorInterface;
 
 class AppendRequestIdProcessor implements ProcessorInterface
@@ -26,7 +27,7 @@ class AppendRequestIdProcessor implements ProcessorInterface
      */
     protected SnowflakeIdGenerator $idGenerator;
 
-    public function __invoke(array $record)
+    public function __invoke(array|LogRecord $record)
     {
         $record['context']['request_id'] = Context::getOrSet(self::REQUEST_ID, $this->idGenerator->generate());
         $record['context']['coroutine_id'] = Coroutine::id();
