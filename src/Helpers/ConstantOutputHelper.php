dd08df5c3d8a32eff635ea3f802ef75a5b24471e
fyi <fyi741@protonmail.com>
2022-08-25 14:46:59 +0800

    升级脚手架模版

---
+++ src/Helpers/ConstantOutputHelper.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date:  2021/11/2
+ * Time:  9:44 下午
+ */
+
+declare(strict_types=1);
+
+namespace App\Module;
+
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Utils\Filesystem\Filesystem;
+use Lengbin\Hyperf\ErrorCode\BaseEnum;
+
+class ConstantOutputHelper
+{
+    #[Inject]
+    protected Filesystem $filesystem;
+
+    /**
+     * enums:
+     *   $path = BASE_PATH . '/app/Constants';
+     *   $this->scan($path, ['Errors'], ['Error']);
+     *
+     * errors:
+     *  $path = BASE_PATH . '/app/Constants/Errors';
+     *  $this->scan($path);
+     *
+     * @param string $dir
+     * @param array $excludeDir
+     * @param array $excludeFile
+     * @param callable|null $call
+     * @return array
+     */
+    public function scan(string $dir, array $excludeDir = [], array $excludeFile = [], ?callable $call = null): array
+    {
+        $data = [];
+        $directories = $this->filesystem->directories($dir);
+        foreach ($directories as $directory) {
+            $directoryName = $this->filesystem->basename($directory);
+            if (in_array($directoryName, $excludeDir)) {
+                continue;
+            }
+            $files = $this->filesystem->allFiles($directory);
+
+            foreach ($files as $file) {
+                $fileName = $file->getBasename('.php');
+                if (in_array($fileName, $excludeFile)) {
+                    continue;
+                }
+                $filePath = substr($file->getRealPath(), 0, -4);
+                /**
+                 * @var BaseEnum $classname
+                 */
+                $classname = implode('\\', array_map(function ($str) {
+                    return ucfirst($str);
+                }, explode('/', str_replace(BASE_PATH, '', $filePath))));
+                $maps = $classname::getMapJson();
+                $data[$directoryName][$fileName] = $call ? call_user_func($call, $maps) : $maps;
+            }
+        }
+        return $data;
+    }
+}
