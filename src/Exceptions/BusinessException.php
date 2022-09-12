14f086e8eb098b313035467febafd274fdb21f05
fyi <fyi741@protonmail.com>
2022-09-12 17:30:19 +0800

    upgrade version

--- src/Exceptions/BusinessException.php
+++ src/Exceptions/BusinessException.php
@@ -12,12 +12,20 @@ declare(strict_types=1);
 namespace Lengbin\Hyperf\Common\Exceptions;
 
 use Hyperf\Server\Exception\ServerException;
+use Lengbin\Hyperf\ErrorCode\BaseEnum;
 use Throwable;
 
 class BusinessException extends ServerException
 {
-    public function __construct(int $code, string $message = null, array $replace = [], Throwable $previous = null)
+    public function __construct(int|BaseEnum $code, string $message = null, array $replace = [], Throwable $previous = null)
     {
+        if ($code instanceof BaseEnum) {
+            if (empty($message)) {
+                $message = $code->getMessage($replace);
+            }
+            $code = $code->getValue();
+        }
+
         if (empty($message)) {
             $config = config('errorCode', []);
             $class = $config['classNamespace'] . '\\' . $config['classname'];
